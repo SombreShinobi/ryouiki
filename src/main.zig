90a4cbe8fa3d4ef64cd39aa3b523eb30e644b5e4
SombreShinobi <sedlan.sibin@gmail.com>
2024-07-14 16:46:46 +0200

    feat: add break loop cmd

--- src/main.zig
+++ src/main.zig
@@ -1,48 +1,72 @@
 const std = @import("std");
 const fs = std.fs;
-const os = std.os;
+const posix = std.posix;
 
 const debug = std.debug;
+
+const Cmd = enum {
+    end,
+    ctn,
+};
 
 pub fn main() !void {
     var tty = try fs.cwd().openFile("/dev/tty", .{ .mode = .read_write });
     defer tty.close();
 
-    const original = try os.tcgetattr(tty.handle);
+    const original = try posix.tcgetattr(tty.handle);
     var raw = original;
-    try goRaw(&tty, &raw);
+    try uncook(&tty, &raw);
 
-    return try nloop(&original, &tty);
+    return try nloop(&original, &raw, &tty);
 }
 
-fn nloop(original: *const os.termios, tty: *fs.File) !void {
+fn nloop(orig: *const posix.termios, raw: *posix.termios, tty: *fs.File) !void {
     while (true) {
-        var buffer: [1]u8 = undefined;
-        _ = try tty.read(&buffer);
-        if (buffer[0] == 'q') {
-            try os.tcsetattr(tty.handle, .FLUSH, original.*);
-            return;
-        } else if (buffer[0] == ':') {
-            try os.tcsetattr(tty.handle, .FLUSH, original.*);
-            return;
+        var buff: [1]u8 = undefined;
+        _ = try tty.read(&buff);
+        if (buff[0] == ':') {
+            try cook(tty, orig);
+            const cmd = try parse(tty);
+            switch (cmd) {
+                .end => break,
+                else => {
+                    try uncook(tty, raw);
+                    debug.print("input: {} {s}\r\n", .{ buff[0], buff });
+                },
+            }
         } else {
-            debug.print("input: {} {s}\r\n", .{ buffer[0], buffer });
+            debug.print("input: {} {s}\r\n", .{ buff[0], buff });
         }
     }
 }
 
-fn goRaw(tty: *fs.File, raw: *os.termios) !void {
-    // TODO: Make the following block os dependant
-    raw.*.lflag &= ~@as(
-        os.darwin.tcflag_t,
-        os.darwin.ECHO | os.darwin.ICANON | os.darwin.ISIG | os.darwin.IEXTEN,
-    );
-    raw.*.iflag &= ~@as(
-        os.darwin.tcflag_t,
-        os.darwin.IXON | os.darwin.ICRNL | os.darwin.BRKINT | os.darwin.INPCK | os.darwin.ISTRIP,
-    );
-    raw.*.cc[os.system.V.TIME] = 0;
-    raw.*.cc[os.system.V.MIN] = 1;
+fn parse(tty: *fs.File) !Cmd {
+    var buff: [1024]u8 = undefined;
+    _ = try tty.read(&buff);
 
-    try os.tcsetattr(tty.handle, .FLUSH, raw.*);
+    if (buff[0] == 'q' and buff[1] == '\n') {
+        return Cmd.end;
+    }
+
+    return Cmd.ctn;
+    // debug.print("buf: {} {s}\r\n", .{ buff[0], buff });
+}
+
+fn uncook(tty: *fs.File, raw: *posix.termios) !void {
+    // TODO: Make the following block os dependant
+    raw.*.lflag.ECHO = false;
+    raw.*.lflag.ICANON = false;
+    raw.*.lflag.ISIG = false;
+    raw.*.lflag.IEXTEN = false;
+    raw.*.iflag.IXON = false;
+    raw.*.iflag.ICRNL = false;
+    raw.*.iflag.BRKINT = false;
+    raw.*.iflag.INPCK = false;
+    raw.*.iflag.ISTRIP = false;
+
+    try posix.tcsetattr(tty.handle, .FLUSH, raw.*);
+}
+
+fn cook(tty: *fs.File, orig: *const posix.termios) !void {
+    try posix.tcsetattr(tty.handle, .FLUSH, orig.*);
 }
