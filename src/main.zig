9cbc21e1cd9b40bd7cf636732e08239a38647e26
SombreShinobi <sedlan.sibin@gmail.com>
2024-08-08 18:53:20 +0200

    feat: cleanup state management + add support for normal, input and cmd modes

--- src/main.zig
+++ src/main.zig
@@ -6,50 +6,104 @@ const debug = std.debug;
 
 const Cmd = enum {
     end,
-    ctn,
+    write,
+};
+
+const State = struct {
+    mode: Mode,
+
+    pub fn init() State {
+        return State{
+            .mode = Mode.insert,
+        };
+    }
+
+    pub fn change_mode(self: *State, new_mode: Mode) void {
+        debug.print("Mode: {}\r\n", .{new_mode});
+        self.mode = new_mode;
+    }
+};
+
+const Mode = enum {
+    insert,
+    cmd,
+    normal,
 };
 
 pub fn main() !void {
     var tty = try fs.cwd().openFile("/dev/tty", .{ .mode = .read_write });
     defer tty.close();
 
+    var state: State = State.init();
+
     const original = try posix.tcgetattr(tty.handle);
     var raw = original;
     try uncook(&tty, &raw);
 
-    return try nloop(&original, &raw, &tty);
+    return try nloop(&state, &original, &tty);
 }
 
-fn nloop(orig: *const posix.termios, raw: *posix.termios, tty: *fs.File) !void {
+fn nloop(state: *State, orig: *const posix.termios, tty: *fs.File) !void {
+    var cmd_buf: [1024]u8 = undefined;
+    var cmd_chars: usize = 0;
     while (true) {
-        var buff: [1]u8 = undefined;
-        _ = try tty.read(&buff);
-        if (buff[0] == ':') {
-            try cook(tty, orig);
-            const cmd = try parse(tty);
-            switch (cmd) {
-                .end => break,
-                else => {
-                    try uncook(tty, raw);
-                    debug.print("input: {} {s}\r\n", .{ buff[0], buff });
-                },
-            }
-        } else {
-            debug.print("input: {} {s}\r\n", .{ buff[0], buff });
+        switch (state.mode) {
+            .insert => {
+                try insert(tty, state);
+            },
+            .normal => {
+                try normal(tty, state);
+            },
+            .cmd => {
+                var buf: [1]u8 = undefined;
+                _ = try tty.read(&buf);
+
+                switch (buf[0]) {
+                    13 => {
+                        // TODO: expand to support multi-char commands
+                        if (cmd_buf[0] == 'q') {
+                            try cook(tty, orig);
+                            break;
+                        } else if (cmd_buf[0] == 'w') {
+                            debug.print("writing to disc!\r\n", .{});
+                        } else {
+                            cmd_chars = 0;
+                            state.change_mode(Mode.normal);
+                        }
+                    },
+                    3 => {
+                        cmd_chars = 0;
+                        state.change_mode(Mode.normal);
+                    },
+                    else => {
+                        cmd_buf[cmd_chars] = buf[0];
+                        cmd_chars += 1;
+                    },
+                }
+            },
         }
     }
 }
 
-fn parse(tty: *fs.File) !Cmd {
-    var buff: [1024]u8 = undefined;
-    _ = try tty.read(&buff);
+fn insert(tty: *fs.File, state: *State) !void {
+    var buf: [1]u8 = undefined;
+    _ = try tty.read(&buf);
+    debug.print("input: {} {s}\r\n", .{ buf[0], buf });
 
-    if (buff[0] == 'q' and buff[1] == '\n') {
-        return Cmd.end;
+    if (buf[0] == 3) {
+        state.change_mode(Mode.normal);
     }
+}
 
-    return Cmd.ctn;
-    // debug.print("buf: {} {s}\r\n", .{ buff[0], buff });
+fn normal(tty: *fs.File, state: *State) !void {
+    var buf: [1]u8 = undefined;
+    _ = try tty.read(&buf);
+
+    switch (buf[0]) {
+        ':' => state.change_mode(Mode.cmd),
+        'i' => state.change_mode(Mode.insert),
+        else => {},
+    }
 }
 
 fn uncook(tty: *fs.File, raw: *posix.termios) !void {
